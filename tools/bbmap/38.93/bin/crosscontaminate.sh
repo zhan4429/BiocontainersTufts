015873cbf8bcfb58734ae54cd5f4c8f3253c2549
Yucheng Zhang <Yucheng.Zhang@tufts.edu>
2024-04-23 09:30:37 -0400

    change module mode and add fqtk

---
+++ tools/bbmap/38.93/bin/crosscontaminate.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+if [ ! $(command -v singularity) ]; then
+        module load singularity/3.8.4 squashfs/4.4
+fi
+
+VER=38.93
+PKG=bbmap
+PROGRAM=crosscontaminate.sh
+DIRECTORY=/cluster/tufts/biocontainers/images
+IMAGE=quay.io_biocontainers_bbmap:38.93--he522d1c_0.sif
+ENTRYPOINT_ARGS=""
+
+## Determine Nvidia GPUs (to pass coresponding flag to Singularity)
+if [[ $(nvidia-smi -L 2>/dev/null) ]]
+then
+        echo "BIOC: Enabling Nvidia GPU support in the container."
+        OPTIONS="--nv"
+fi
+	
+singularity exec $OPTIONS $DIRECTORY/$IMAGE $ENTRYPOINT_ARGS $PROGRAM "$@"
